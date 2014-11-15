9749465914d64327b4c5c79ad517e564ce63fef9
Pawel Nowosielski <nowosielski@gmail.com>
2014-11-15 20:52:12 +0000

    Added GA code

--- index.php
+++ index.php
@@ -317,4 +317,14 @@ This is a test service: Data may be deleted anytime. Kittens will die if you abu
 <!-- End of scripts for client-side decryption -->
 
 </body>
+  <!-- GA code -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-56783468-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 </html>
