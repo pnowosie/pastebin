3c28daa24a03cd2432641b76ef97eb1c3f59427f
Pawel Nowosielski <nowosielski@gmail.com>
2015-02-09 10:06:39 +0100

    Configuration file

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+    $config = array(
+        // application run mode
+        'mode' => 'PRODUCTION',
+
+        // application root directory
+        'AppDir' => '',
+
+        // database settings
+        'db/connStr' => 'YOUR_DATABASE_CONNECTION_STRING',
+        'db/user' => 'YOUR_DATABASE_USER',
+        'db/pass' => 'YOUR_DATABASE_PASSWORD',
+    );
+
+?>
